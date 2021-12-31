15e0687060bad692bc7292837e47ff7b60bc165d
Micro-Prog <76631496+Micro-Prog@users.noreply.github.com>
2021-12-31 20:29:01 +0330

    Add files via upload

---
+++ Python Script-Battery Tracker/Main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 31 19:19:21 2021
+
+@author: sajjad
+"""
+
+
+import os
+import time
+
+def CheckPower(percentage):
+    if percentage < 10:
+        return True
+    else:
+        return False
+
+
+def Shutdown(Check):
+    if Check == True:
+        print("The Battery Power is Below 10%")
+        time.sleep(5)
+        os.system("reboot") # "shutdown" for power off
+        exit()
+    else:
+        print("Battery Power is above 10%")
+
+
+def readBattery():
+    op = open("/sys/class/power_supply/BAT0/capacity", "r")
+    capacity = op.readline().strip()
+    return capacity
+
+
+while True:
+    stat = int(readBattery())
+    print("Battery Power: " , f'{stat}%')
+    CheckTrue = CheckPower(stat)
+    Shutdown(CheckTrue)
+    time.sleep(30)
+
+
+
+
+
+
+
+
+
+
+
+
